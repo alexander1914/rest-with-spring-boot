172181d436b0b66734644cb91c25e4677d52d267
Alexander Oliveira <acc01719@sky.com.br>
2023-02-27 21:27:52 -0300

    Implementando o migration com flyway

--- src/main/java/br/com/udemy/controllers/PersonController.java
+++ src/main/java/br/com/udemy/controllers/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
     //@Autowired: é para fazer a instaciação de um objeto,
